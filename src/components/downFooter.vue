7e771d362c3d2c0976daed50165c5c4a231d7fb1
GiulianoCretella <giulianocretella.1@gmail.com>
2022-05-04 23:01:38 +0200

    downFooter completo

--- src/components/downFooter.vue
+++ src/components/downFooter.vue
@@ -1,16 +1,92 @@
 <template>
-    <div class="down-footer"></div>
+   <div class="down-footer">
+      <div class="container-down-footer">
+         <span>SIGN-UP NOW!</span>
+         <div class="contacts">
+            <span>FOLLOW US</span>
+            <ul>
+               <li v-for="(item,index) in social" :key="index">
+                  <img :src="item.immagine" alt="">
+               </li>
+            </ul>
+         </div>
+      </div>
+   </div>
 </template>
 <script>
 export default {
-   name:'DownFooter'
+   name:'DownFooter',
+   data(){
+      return {
+         social:[
+            {
+               nome:'facebook',
+               link:'',
+               immagine:require('../assets/img/footer-facebook.png')
+            },
+            {
+               nome:'twitter',
+               link:'',
+               immagine:require('../assets/img/footer-twitter.png')
+            },
+            {
+               nome:'youtube',
+               link:'',
+               immagine:require('../assets/img/footer-youtube.png')
+            },
+            {
+               nome:'pinterest',
+               link:'',
+               immagine:require('../assets/img/footer-pinterest.png')
+            },
+            {
+               nome:'periscope',
+               link:'',
+               immagine:require('../assets/img/footer-periscope.png')
+            }
+         ]
+      }
+   }
 }
 </script>
 <style lang="scss">
 @import '../style/variables.scss';
 
 .down-footer{
+   padding: 10px;
    background-color: $dc_darkGrey;
-   height: 80px;
+   .container-down-footer{
+      @include container_width;
+      @include flexy-center;
+      justify-content: space-between;
+      span{
+         border: 1px solid $dc_mainBlue;
+         padding: 5px;
+         font-size: 0.7em;
+         color:$dc_title;
+      }
+      .contacts{
+         @include flexy-center;
+         span{
+            color:$dc_mainBlue;
+            font-weight: bold;
+            border: none;
+            font-size: 1em;
+         }
+         ul{
+            padding-left: 10px;
+            display: flex;
+            li{
+               list-style-type:none;
+               padding: 10px;
+               img{
+                  max-width: 25px;
+               }
+            }
+         }  
+      }
+      
+
+   }
 }
 </style>
