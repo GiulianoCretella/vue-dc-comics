d01ffa51f2d39446439d3ed2d8a93cb3f9dcaf72
GiulianoCretella <giulianocretella.1@gmail.com>
2022-05-05 16:19:42 +0200

    jumbo e card nel main dinamicamente

---
+++ src/components/MainCard.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="product-card">
+        <div class="card">
+             <img :src="thumb" alt="titolo">
+        </div>
+             <h3>{{titolo}}</h3>
+
+    </div>
+</template>
+<script>
+export default {
+    nome:'ProductCard',
+    data(){
+        return{
+
+        }
+
+    },
+    props:['thumb','titolo']
+}
+</script>
+<style lang="scss" scoped>
+@import '../style/variables';
+
+    .product-card{
+        width: calc(100% / 6 - 10px);
+        padding: 5px;
+        margin: 5px;
+        .card{
+            width: 100%;
+            height: 150px;
+            overflow: hidden;
+            img{
+                max-width: 100%;
+            }
+        }
+        h3{
+            text-align: center;
+            font-size: 1em;
+            color:$dc_title;
+            width: 100%;
+        }
+    }
+</style>
